312814ced2567522df058aae5f4d4f39e887a858
Chinazo <chinazoonwukaike@gmail.com>
2025-03-28 14:09:38 -0500

    remove extra space from ThemedButton

--- presentation/shared/ThemedButton.tsx
+++ presentation/shared/ThemedButton.tsx
@@ -13,7 +13,6 @@ const ThemedButton = ({ className, children, ...rest }: ThemedButtonType) => {
         className={`bg-light-primary dark:bg-dark-primary items-center rounded-xl px-6 py-2 active:opacity-80 ${className}`}
         {...rest}
       >
-        {" "}
         <Text className="text-white text-2xl">{children}</Text>
       </Pressable>
     </View>
